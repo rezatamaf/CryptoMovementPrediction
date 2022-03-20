e1d0d66b66af79ce811555356afc771a4bdda04b
rezatamaf <rezatama.fathullah@gdplabs.id>
2022-03-20 21:38:28 +0700

    add fundamental data loader

--- price_movement/data_loader.py
+++ price_movement/data_loader.py
@@ -1,9 +1,87 @@
+import json
+import requests
 import datetime as dt
 import numpy as np
 import pandas as pd
+from iso8601 import ParseError, parse_date
 
 from price_movement.util import Utils
 from price_movement.feature_processor import SentimentProcessor
+
+
+class GlassnodeClient:
+
+    def __init__(self):
+        self._api_key = ''
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    def set_api_key(self, value):
+        self._api_key = value
+
+    def get(self, url, a='BTC', i='24h', c='native', s=None, u=None):
+        p = dict()
+        p['a'] = a
+        p['i'] = i
+        p['c'] = c
+
+        if s is not None:
+            try:
+                p['s'] = parse_date(s).strftime('%s')
+            except ParseError:
+                p['s'] = s
+
+        if u is not None:
+            try:
+                p['u'] = parse_date(u).strftime('%s')
+            except ParseError:
+                p['u'] = s
+
+        p['api_key'] = self.api_key
+
+        r = requests.get(url, params=p)
+
+        try:
+            r.raise_for_status()
+        except Exception as e:
+            print(e)
+            print(r.text)
+
+        try:
+            df = pd.DataFrame(json.loads(r.text))
+            df = df.set_index('t')
+            df.index = pd.to_datetime(df.index, unit='s')
+            df = df.sort_index()
+            s = df.v
+            s.name = '_'.join(url.split('/')[-2:])
+            return s
+        except Exception as e:
+            print(e)
+
+
+class FundamentalMetricLoader:
+    def __init__(self, reference_date_idx: pd.Index, api_key: str,
+                 base_url='https://api.glassnode.com/v1/metrics'):
+        self.reference_date_idx = reference_date_idx
+        self.base_url = base_url
+        self.client = self._authorize(api_key)
+
+    @staticmethod
+    def _authorize(api_key: str):
+        client = GlassnodeClient()
+        client.set_api_key(api_key)
+        return client
+
+    def get_metric(self, end_point):
+        since_date, until_date = self._get_date_range()
+        return self.client.get(f'{self.base_url}/{end_point}', s=since_date, u=until_date).astype('float64')
+
+    def _get_date_range(self):
+        min_date = Utils.datetime_to_str(self.reference_date_idx.min())
+        max_date = Utils.datetime_to_str(self.reference_date_idx.max() + dt.timedelta(1))  # api use exclusive boundary
+        return min_date, max_date
 
 
 class DataLoader:
@@ -12,13 +90,15 @@ class DataLoader:
                  cnn_sentiment_dir: str,
                  btcnews_sentiment_dir: str,
                  gtrend_dir: str,
-                 binance_price_dir: str
+                 binance_price_dir: str,
+                 glassnode_api_key: str
                  ):
         self.twitter_sentiment_dir = twitter_sentiment_dir
         self.cnn_sentiment_dir = cnn_sentiment_dir
         self.btcnews_sentiment_dir = btcnews_sentiment_dir
         self.gtrend_dir = gtrend_dir
         self.binance_price_dir = binance_price_dir
+        self.glassnode_api_key = glassnode_api_key
         self.twitter_positive_sentiment = 0
         self.twitter_negative_sentiment = 0
         self.news_sentiment = 0
@@ -31,10 +111,11 @@ class DataLoader:
         cnn_df = self._load_sentiment(self.cnn_sentiment_dir, training_period, today_reference)
         gtrend_df = self._load_gtrend(self.gtrend_dir, training_period, today_reference)
         btcnews_df = self._load_sentiment(self.btcnews_sentiment_dir, training_period, today_reference)
+        fundamental_df = self._load_fundamental(self.glassnode_api_key, twitter_df.index)
         price_df = self._load_price(self.binance_price_dir, training_period, today_reference)
 
         # concat all data to one dataframe
-        dfs = [twitter_df, cnn_df, btcnews_df, gtrend_df, price_df]
+        dfs = [twitter_df, cnn_df, btcnews_df, gtrend_df, fundamental_df, price_df]
         data_df = pd.concat(dfs, join='outer', axis=1).fillna(0)[1:]  # drop 1st row after lagged preprocessing
         complete_date_idx = Utils.get_relevant_dates(training_period, today_reference)[1:]
         data_df = data_df.reindex(complete_date_idx).fillna(0)  # add missing date if any
@@ -78,3 +159,13 @@ class DataLoader:
         gtrend_df.index = adj_index
         gtrend_df.index.name = 'date'
         return gtrend_df
+
+    @staticmethod
+    def _load_fundamental(api_key: str, reference_date_idx: pd.Index):
+        metric_loader = FundamentalMetricLoader(reference_date_idx, api_key)
+        end_points = ['indicators/sopr', 'transactions/rate', 'mining/hash_rate_mean', 'addresses/active_count',
+                      'blockchain/utxo_created_value_sum', 'blockchain/utxo_spent_value_sum']
+        df = pd.DataFrame({i.split("/")[1]: metric_loader.get_metric(i) for i in end_points})
+        df.index.names = ['date']
+        return df
+
